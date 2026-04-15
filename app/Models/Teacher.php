795a4f37d80bbe5bc62af6dfe3b2435b2499ac85
eduardocamacho4977-eng <eduardocamacho4977@gmail.com>
2026-04-14 21:41:30 -0500

    Add relationships for area, training center, and courses

--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Teacher extends Model
 {
     use HasFactory;
+
+    public function area(){
+        return $this->belongsTo(Area::class);
+    }
+
+    public function trainingCenter(){
+        return $this->belongsTo(TrainingCenter::class);
+    }
+
+    public function courses(){
+        return $this->belongsToMany(Course::class);
+    }
 }
