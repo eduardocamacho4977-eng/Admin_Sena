f4575e553e5da8ee3ababbe815d095084ebc3367
eduardocamacho4977-eng <eduardocamacho4977@gmail.com>
2026-04-14 21:42:53 -0500

    Rename Training-Center class and add relationships
    Renamed class from 'Training-Center' to 'Training_Center' and added relationships for teachers and courses.

--- app/Models/Training-Center.php
+++ app/Models/Training-Center.php
@@ -5,7 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Training-Center extends Model
+class Training_Center extends Model
 {
     use HasFactory;
+
+    public function teachers(){
+        return $this->hasMany(Teacher::class);
+    }
+
+    public function courses(){
+        return $this->hasMany(Course::class);
+    }
 }
